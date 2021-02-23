ef7dbbaee40bbcc591addd45eb040ae604746a16
x35gaming <44137552+x35gaming@users.noreply.github.com>
2021-02-23 10:26:09 +0800

    changed password hash system, less clunky

--- admin/passhash.php
+++ admin/passhash.php
@@ -6,12 +6,21 @@
 * (C) X35gaming, under GNU GPL-v3
 * */
 session_start();
-?>
-<form method="post">
-    <input type="password" placeholder="password" name="passwd">
-    <input type="submit" value="go">
-</form>
-<?php
-if (isset($_POST["passwd"])) {
-echo password_hash($_POST["passwd"],1);
-};
+$conf = json_decode(file_get_contents("../config.json"),true); // load config file.
+if (isset($_SESSION["passwd"])) {
+    if(password_verify($_SESSION["passwd"],$conf["password-hash"])){
+        $conf;        // declare vars
+        echo '
+        <form method="post">
+            <input type="password" placeholder="password" name="passwd">
+            <input type="submit" value="go">
+        </form>
+        ';
+        if (isset($_POST["passwd"])) {
+            $conf['password-hash'] = password_hash($_POST["passwd"],1);
+            $newpost=$conf;
+            file_put_contents("../config.json", json_encode($newpost,JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES));
+            echo "hash changed successfully";
+        }
+    }
+}
